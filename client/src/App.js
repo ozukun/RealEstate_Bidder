3508cdbe85a99ecc660c7b49e787f296f021b032
ozukun <ozgurb83@gmail.com>
2021-05-28 00:22:48 +0300

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,18 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    const HouseList=[["0x9A00348361887FF759c277451150fCc02737A671",10],
-    ["0xcB41eFBfcC89068EeDa8fBDafE4B914204C9b506",5],
-    ["0x3bD574fF3D4a4FDde865804968B48a8863EDC1C5",20] ]
 
-    this.state = { storageValue: 0, web3: null, accounts: null, contract: null ,Hlist: HouseList };
+    this.state = { 
+      storageValue: 0, 
+      web3: null, 
+      accounts: null, 
+      contract: null,
+      Hlist: [
+        ["0x9A00348361887FF759c277451150fCc02737A671",10,"false"],
+        ["0xcB41eFBfcC89068EeDa8fBDafE4B914204C9b506",5,"false"],
+        ["0x3bD574fF3D4a4FDde865804968B48a8863EDC1C5",20,"false"]
+       ] 
+    };
 
     this.handleChange = this.handleChange.bind(this);
    
@@ -38,6 +45,8 @@ class App extends Component {
         SimpleStorageContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
+      
+  
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
@@ -62,10 +71,11 @@ class App extends Component {
     // Get the value from the contract to prove it worked.
     //const response = await contract.methods.get().call();
     
-    const eth_amount=document.getElementById("input_eth").value*10**18;
+    const eth_amount=document.getElementById("input_eth").value;
     const rcv_address=document.getElementById("input_rcv").value;
 
-    const load_up={  
+    const load_up=
+    {  
       //to: '0x9A00348361887FF759c277451150fCc02737A671',
       from: accounts[0],
       //gas: 50000, gasPrice: 1e6 ,
@@ -73,10 +83,43 @@ class App extends Component {
     };
     
     console.log(accounts[0]);
-    await contract.methods.sendEth(rcv_address).send(load_up);
-    console.log("done !!");
+    //await contract.methods.sendEth(rcv_address).send(load_up);
+    
+    var i;
+    for (i = 0; i < this.state.Hlist.length; i++) 
+    {
+      if (rcv_address===this.state.Hlist[i][0])
+      {
+         console.log(this.state.Hlist[i][0]);
+         console.log(this.state.Hlist[i][1]);
+         console.log("%%%%");
+         console.log(eth_amount);
+         if(parseInt(eth_amount, 10)>= parseInt(this.state.Hlist[i][1],10)  )
+         {
+          await contract.methods.placeBid(this.state.Hlist[i][0],eth_amount).send({ from: accounts[0] });
+          console.log("Bid Completed !!")
+          //await contract.methods.setdummy().send({ from: accounts[0] });
+          //let x=await contract.methods.dummy.call().call();
+          //console.log(x);
+          let Highest_Bid= await contract.methods.getHighestBid(this.state.Hlist[i][0]).call();
+          let array_tmp=this.state.Hlist.slice();
+          array_tmp[i][1]=Highest_Bid.toString();
+          array_tmp[i][2]="true";
+          this.setState({ Hlist: array_tmp });
 
-
+         }
+      } 
+    } 
+    
+    
+    ///await contract.methods.placeBid(accounts[0],eth_amount).send({ from: accounts[0] });
+    ////console.log("done !!");
+     
+    
+    /*const x = await  contract.methods.getBidder().call();
+    console.log(x);
+    const x2 =await  contract.methods.getBidPrice().call();
+    console.log(x2);*/
 
     // Update state with the result.
     //this.setState({ storageValue: response });
@@ -90,19 +133,47 @@ class App extends Component {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
+    var i;
+    for (i = 0; i < this.state.Hlist.length; i++) 
+    {
+      if (this.state.Hlist[i][2]==="true" && i===0)
+      {
+        document.getElementById('mydiv0').className = 'price_change';
+      }
+      else if(this.state.Hlist[i][2]==="true" && i===1)
+      {
+        document.getElementById('mydiv1').className = 'price_change';
+      }
+      else if(this.state.Hlist[i][2]==="true" && i===2)
+      {
+        document.getElementById('mydiv2').className = 'price_change';
+      }
+    }
+
+
+
     return (
+
       <div className="App">
          <div className="Title">REAL ESTATE BIDDING</div>
          <div className="HouseContainer" >
             <div className="House"><img src={ReactHouse1} alt="React Logo" />
             <div class="caption">House1: {"\n"}{this.state.Hlist[0][0]} {"\n"}
-            Price: {"\n"}{this.state.Hlist[0][1]} Ether</div></div>
+            <div id="mydiv0" class="price" >Price: {"\n"}{this.state.Hlist[0][1]} Ether </div>
+            </div></div>
+
             <div className="House"><img src={ReactHouse2} alt="React Logo" />
-            <div class="caption">House2: {"\n"}{this.state.Hlist[1][0]} {"\n"}
-            Price: {"\n"}{this.state.Hlist[1][1]} Ether </div></div>
+            <div class="caption">House2: {"\n"}{this.state.Hlist[1][0]} {"\n"} 
+            <div id="mydiv1" class="price" >Price: {"\n"}{this.state.Hlist[1][1]} Ether </div>
+            </div></div>
+
             <div className="House"><img src={ReactHouse3} alt="React Logo" />
             <div class="caption">House3: {"\n"}{this.state.Hlist[2][0]} {"\n"}
-            Price: {"\n"}{this.state.Hlist[2][1]} Ether </div></div>
+            <div id="mydiv2" class="price" >Price: {"\n"}{this.state.Hlist[2][1]} Ether </div>
+            </div></div>
+            
+            
+
             
         </div>
         <div className="App-input" >
@@ -113,7 +184,7 @@ class App extends Component {
           <div className="App-msg">Sender account address and ETH amount :</div>
           <input type="text" id="input" value={this.state.accounts[0]} ></input>
           <input type="text" id="input_eth" value={this.state.storageValue} onChange={this.handleChange} className="App-input-eth"  ></input>
-          <div><button type="submit" class="btn btn-sm btn-warning" onClick={()=>this.runExample()}>Submit</button></div>
+          <div><button type="submit" class="btn btn-sm btn-warning" onClick={()=>this.runExample()}>BID</button></div>
 
         </div>
       </div>
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,21 +1,55 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.21 <=0.8.4;
 
-contract SimpleStorage {
-  uint storedData;
+//import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-  function set(uint x) public {
-    storedData = x;
+contract SimpleStorage  {
+  //using SafeMath for uint;
+
+
+  mapping(address => address)  highest_Bidder; // sell_item -> bidder address
+  mapping(address => uint256)  bid_Transaction; // sell_item --> bid price
+  address payable  bidder;
+  uint256 public contract_amount ;
+  uint256 public dummy =44;
+
+
+
+  function placeBid(address seller,uint256 bid) public
+  {     
+        //bid=bid*10**18;
+        //require(bid<=contract_amount,"insufficient balance");
+        //require(bid>bid_Transaction[seller],"Bid should be higher than current one");
+        bid_Transaction[seller]=bid;
+        highest_Bidder[seller]=bidder;
+        dummy=333;
+        
+  } 
+  function setdummy() public view returns(uint256)
+  {
+     return dummy;
+  }
+  function getHighestBid(address seller) public view returns(uint256)
+  {
+      
+      return bid_Transaction[seller];
   }
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
-
-  function sendEth(address payable rcv_addr) public  payable
+  /*function sendEth(address payable rcv_addr) public  payable
   {
     address payable aa=rcv_addr;
     uint256 xs=msg.value;
     aa.transfer(xs);
+  
+  }*/
+
+    function completeAuction(address payable seller) public  payable
+  {
+      seller.transfer(bid_Transaction[seller]);
+      contract_amount=contract_amount-bid_Transaction[seller];
   }
+
+
+
+
 }
